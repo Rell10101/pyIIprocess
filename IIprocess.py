b6e57df207b7b83ca50b4383e7f7328b033b2fac
Rell10101 <anton.samaev321@mail.ru>
2024-04-09 23:57:37 +0900

    update

--- IIprocess.py
+++ IIprocess.py
@@ -1,43 +1,61 @@
-__author__ = "Самаев Антон ИВТ-21"
-
 import multiprocessing
-import random
+from multiprocessing import Queue, Array, Value
+from multiprocessing import Process
+
+
 import time
 
-# процессы не имеют общей памяти
-# они могут взаимодействовать друг с другом [неявно] используя средства ОС
-# зато, в отличии от потоков в Python, они могут выполнятся ||
+
 
 #в процессах блокировщик нужно явно передавать, чтобы использовать его
-def add_value(locker, array, index):
+def add_value(locker, data: Array, start, end: Value, result: Value):
     with locker:
-        num = random.randint(0, 20)
-        vtime = time.ctime() #текущее время
-        array[index] = num
-        print(f"array[{index}] = {num}, sleep = {vtime}")
-        time.sleep(1)
+        sum = 0
+        # result.value = 0
+        for i in range(start, end):
+            sum = sum + data[i]
+        result.value = sum
 
 
-#Это надо для того, чтобы наш модуль можно было безопасно подключать в другие модули и при этом не создавались новые процессы без нашего ведома
+def fill_array(arr: Array):
+		for i in range(len(arr)):
+				arr[i] = 1
+
+
 if __name__ == '__main__':
 
-    #mutex
     lock = multiprocessing.Lock()
 
-    #массив из 10 элементов типа int ("i"), размещается в общей для процессов памяти
-    arr = multiprocessing.Array("i", range(10))
+    N = int(5000000)
+    arr = Array('d', N)					# массив из N элементов типа double ('d'), размещается в общей для процессов памяти
 
-    #список процессов, используется в дальнейшем для join()
-    process = []
+    print('Array initializing...')
+    fill_array(arr)
 
-    for i in range(10):
-        pr = multiprocessing.Process(target=add_value, args=(lock, arr, i, ))
-        process.append(pr)
-        pr.start()
-
-    #ожидание завершения работы всех процессов
-    for i in process:
-        i.join()
+    S1 = Value('d', 0.0)				# объект размещённый в общей памяти, хранит значение типа double
+    S2 = Value('d', 0.0)
+    S3 = Value('d', 0.0)  # объект размещённый в общей памяти, хранит значение типа double
 
 
-    print(list(arr))
+
+    print('Calculating...')
+    t0 = time.time()
+
+
+    proc1 = Process(target = add_value, args=(lock, arr, 0, N//4, S1))
+    proc2 = Process(target=add_value, args=(lock, arr, N//4, N // 2, S2))
+    proc3 = Process(target=add_value, args=(lock, arr, N // 2, N, S3))
+    proc1.start()
+    proc2.start()
+    proc3.start()
+
+    proc1.join()
+    proc2.join()
+    proc3.join()
+
+
+    print(f'Fineshed in {(time.time()-t0)} seconds')
+    S = S1.value + S2.value + S3.value
+
+    print(f'sum = {S}')
+
